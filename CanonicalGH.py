3f4781b170a241a27efb5b26c49060e52fccb9ba
chandlerstevenson <125477699+chandlerstevenson@users.noreply.github.com>
2023-04-19 08:45:57 -0400

    Update CanonicalGH.py

--- CanonicalGH.py
+++ CanonicalGH.py
@@ -1,78 +1,33 @@
-import random
-import numpy as np
+import numpy as np 
 
-def generator_matrix(K, N, thresh):
-    Q = N - K
+np.set_printoptions(threshold=np.inf)
+def create_canonical_matrices(n, k, density):
+    if n <= k:
+        raise ValueError("n must be greater than k")
+    if density < 0 or density > 1:
+        raise ValueError("density must be between 0 and 1")
 
-    G = np.zeros((K, N), dtype=int)
+    # Create the canonical generator matrix G
+    P_continuous = np.random.rand(k, n-k)
+    P = (P_continuous < density).astype(int)
+    G = np.hstack((np.eye(k, dtype=int), P))
 
-    # Identity portion
-    for n in range(N-K, N):
-        for k in range(K):
-            if k == n - (N-K):
-                G[k, n] = 1
+    # Create the parity check matrix H
+    H = np.hstack((P.T, np.eye(n-k, dtype=int)))
 
-    # Parity portion
-    for k in range(K):
-        j = random.randint(N-K, N-1)
-        G[k, j] = 1
+    # Verify GH is equal to the zero matrix
+    product = np.matmul(G, H.T) % 2
+    assert np.array_equal(product, np.zeros((k, n-k), dtype=int)), "GH is not equal to the zero matrix"
 
-    for n in range(N-K):
-        for k in range(K):
-            # Skip the presets
-            if G[k, n] != 1:
-                if random.random() < thresh:
-                    G[k, n] = 1
+    return G, H
 
-    return G
+n = 6 #User input
+k = 3 #User input
+density = 0.6 #User input
+G, H = create_canonical_matrices(n, k, density)
 
-def parity_check_matrix(K, N, thresh):
-    Q = N - K
+print("Generator matrix G:")
+print(G)
 
-    G = generator_matrix(K, N, thresh)
-    H = np.zeros((Q, N), dtype=int)
-
-    for p in range(Q):
-        for n in range(Q):
-            if p == n:
-                H[p, n] = 1
-
-    for p in range(Q):
-        for n in range(N-K, N):
-            H[p, n] = G[n-(N-K), p]
-
-    return H
-
-# Example usage
-K = 3
-N = 6
-thresh = 0.5
-
-G = generator_matrix(K, N, thresh)
-H = parity_check_matrix(K, N, thresh)
-
-GH = np.mod(np.matmul(G, H.T), 2)
-
-print("G:\n", G)
-print("H:\n", H)
-print("GH:\n", GH)
-
-
-def encode_messages(messages, Gen):
-    """
-    Encode an array of n bit messages by multiplying each one by a generator matrix G.
-
-    Args:
-        messages: An array of n bit messages.
-        G: A generator matrix.
-
-    Returns:
-        An array of codewords produced by encoding each message with the generator matrix G.
-    """
-    codewords = np.dot(Gen, messages) % 2
-    return codewords
- 
-codeword = np.dot(G.T, [1, 1, 1])  
-print(np.mod(codeword, 2))
-check = np.mod(np.dot(H, codeword), 2)
-print(check)
+print("\nParity check matrix H:")
+print(H) 
